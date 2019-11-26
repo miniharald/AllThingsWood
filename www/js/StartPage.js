08a2f36302863763c530bea058974e5ba086a211
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2019-11-26 11:56:14 +0100

    Update StartPage.js
    changed text in p

--- www/js/StartPage.js
+++ www/js/StartPage.js
@@ -5,7 +5,10 @@ class StartPage {
         <div class="col">
           <img class="img-fluid" src="images/woods.jpeg">
           <h1 class="text-dark text-center mt-3">Welcome to All Things Wood!</h1>
-          <p class="text-dark text-center">Välkommen till vår fina webbsida!</p>
+          <p class="text-dark text-center">
+          Do you want to do your part for mother nature? Buy our handmade stuff
+          , all made in wood!
+          </p>
         </div>
       </section>
     `);
