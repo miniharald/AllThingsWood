11755b5f98c8ac2e8253e8d1692b26bdad44a525
Zero <razviy69@gmail.com>
2019-11-28 09:07:48 +0100

    No :')

--- www/js/Cart.js
+++ www/js/Cart.js
@@ -49,7 +49,7 @@ class Cart {
 
   
     // alert(`
-    //   I am the cart. I'm still really stupid 😢!
+    //   I am the cart. I'm still really stupid 
     //   I have no render-method and no methods that calc sums.
     //   I have no add and remove methods...
     //   But I know that you tried to add this product to me:
