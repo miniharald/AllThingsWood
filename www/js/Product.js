cd5bfa1be63622249cabdda583e31b9b25f2d178
Zero <razviy69@gmail.com>
2019-12-05 14:59:28 +0100

    Animation-prepared

--- www/js/Product.js
+++ www/js/Product.js
@@ -16,7 +16,6 @@ class Product {
       this.animationSound();
     });
   }
-
   render() {
     $('main').html(/*html*/`
      <section class="row">
@@ -56,8 +55,7 @@ animation(id){
 
 
 let cart = $('.fa-shopping-cart');
-let imgtodrag = $(".item").find(`img-${id}`);
-console.log(id);
+let imgtodrag = $(".item").find(`#img-${id}`);
 
 // if(!imgtodrag.lenght){
 //   console.log("in if")
@@ -98,50 +96,7 @@ if (imgtodrag) {
 }
 
 }
-animation(id){
-
-let cart = $('.fa-shopping-cart');
-let imgtodrag = $(".item").find(`img-${id}`);
-
-if(!imgtodrag.lenght){
-  console.log("in if")
-  imgtodrag = $(".item").find("img").eq(0);}
-
-if (imgtodrag) {
-    var imgclone = imgtodrag.clone().offset({
-        top : imgtodrag.offset().top,
-        left : imgtodrag.offset().left
-    })
-        .css({
-        'opacity': '0.5',
-            'position': 'absolute',
-            'height': '150px',
-            'width': '150px',
-            'z-index': '100'
-    })
-        .appendTo($('main'))
-        .animate({
-        'top': cart.offset().top + 10,
-            'left': cart.offset().left + 10,
-            'width': 75,
-            'height': 75
-    }, 1000, 'easeInOutExpo');
-    
-    setTimeout(function () {
-        cart.effect("shake", {
-            times: 2
-        }, 200);
-    }, 1900);
-
-    imgclone.animate({
-        'width': 0,
-            'height': 0
-    }, function () {
-        $(this).detach()
-    });
-}
-
-}
+ 
 animationSound(){
   let player = new Audio();
   player.src = "https://actions.google.com/sounds/v1/cartoon/siren_whistle.ogg";
