c8a251fc1f61188045ceb11c34eb0a58b3ea24c5
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2019-12-11 16:13:58 +0100

    version 1.1

--- www/js/OrderHistoryList.js
+++ www/js/OrderHistoryList.js
@@ -2,15 +2,34 @@ class OrderHistoryList {
 
     rowOutput() {
         let output = " "
-        for(let i in store.orderHistory){
-        output += `<section class="row font-weight-bolder listBorder py-2">
-                     <section class="col-2">${store.orderHistory[i].date}</section>
-                     <section class="col-4">${store.orderHistory[i].orderNr}</section>
-                     <section class="col-2">${store.orderHistory[i].name}</section>
+        console.log(store.orderHistory)
+        for (let order of store.orderHistory) {
+            output += `<section class="row font-weight-bolder listBorder py-2">
+                     <section class="col-2">${order.date}</section>
+                     <section class="col-4">${order.orderNr}</section>
+                     <section class="col-2">Antal köpta produkter</section>
                      <section class="col-2">Total kostnad</section>
                      <section class="col-2"><i class="fa fa-chevron-down"></i></section>
-                  </section>`;
-                }
+                  </section>
+                  `
+
+            for (let product of order.productList.products) {
+                output += `<section class="">
+      <section class="row listBorder">
+        <section class="col-2"></section>
+        <section class="col-6 text-dark font-weight-bolder"></section>
+        <section class="col-2 text-dark font-weight-bolder">Quantity</section>
+        <section class="col-2 text-dark font-weight-bolder">á Price</section>
+      </section>
+      <section class="row listBorder">
+        <section class="col-2"><img class="border border-primary rounded list align-middle" src="${product.image}"></section>
+        <section class="col-6 text-dark font-weight-bolder">PRODUCT NAME & SHORT HERE</section>
+        <section class="col-2 text-dark font-weight-bolder">PRODUCT QUANTITY HERE</section>
+        <section class="col-2 text-dark font-weight-bolder">PRODUCT PRICE (NOT TOTAL) HERE</section>
+      </section>`
+            }
+        }
+        //console.log(output)
         //    for (let i in store.products) {
         //        output += `<section class="row">
         //                     <section class="col">Datum</section>
