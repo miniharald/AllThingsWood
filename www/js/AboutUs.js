82f1d146de62666dd63b5a3bd98f86b794c60ec4
Alexander Persson <55090569+miniharald@users.noreply.github.com>
2019-12-12 17:43:33 +0100

    alex variant

--- www/js/AboutUs.js
+++ www/js/AboutUs.js
@@ -2,13 +2,12 @@ class AboutUs {
 
   render() {
     $('main').html(/*html*/`
-      <section class="row fontSize ">
+      <section class="row">
 
         <div class="col text-center">
-        <img class=" mainRow logo1 img-fluid pb-3" src="/images/betalogoallthingswood.png" alt="">
+        <img class=" logo1 img-fluid pb-2" src="/images/betalogoallthingswood.png" alt="">
 
         
-        <h2>Welcome to All Things Wood!</h2>
         <p class="font-weight-bolder">We are a small company operating  ipsum dolor sit, amet consectetur adipisicing elit. <i> Reiciendis aliquid id doloribus tenetur? </i>  We feel great about you reading this section of our webpage, please say hi!</p>
         <p class="font-weight-bolder">
         You can reach us on any of our fascinating socials! Come say hi!</p>
